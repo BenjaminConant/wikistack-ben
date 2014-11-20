7d1be8d5ca570d99fa5efb531b47e4a312fd006f
Benjamin Conant <conantbenjamin@gmail.com>
2014-11-20 09:44:49 -0500

    hello

---
+++ routes/add.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var Page = require('../models/').Page;
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+	console.log("serve up some wikiwikistackstack add page!!");
+	res.render('add', { title: 'wikiwikistackstack'});
+});
+
+router.post('/submit', function(req, res) {
+	console.log("now add the page once it is submited!!");
+	var postTitle = req.body['post-title'];
+	var postContent = req.body['post-content'];
+	var postUrl = postTitle.replace(/\s/g, "_");
+
+	console.log(postTitle + "----------" + postContent + "---------" + postUrl);
+	
+ 
+  // STUDENT ASSIGNMENT:
+  
+  	var p = new Page({ "title": postTitle, "body":postContent, "url_name": postUrl});
+ 	p.save();
+  	res.redirect('/');
+});
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var Page = require('../models/').Page;
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  Page.find({},function(err, data) {
+	 	console.log('----------------------',data)
+		if (err) return console.error(err);
+		res.render('index', { title: "wikiwikistackstack", docs: "data" });
+	 })
+});
+
+module.exports = router;
